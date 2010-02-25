70a3fdf9028d48d4750f20502408bdc4ffc323f0
useless (MB) <xlii@gmx.de>
2010-02-25 16:10:26 +0100

    Initial

---
+++ UCChangesDocument.h
@@ -0,0 +1,19 @@
+//
+//  MyDocument.h
+//  Changes
+//
+//  Created by Christoph on 25.02.2010.
+//  Copyright 2010 Useless Coding. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+typedef enum {
+	UC_FOLDER_ERROR
+} UCChangesErrors;
+
+@interface UCChangesDocument : NSDocument
+{
+}
+@end
---
+++ UCChangesDocument.m
@@ -0,0 +1,48 @@
+//
+//  MyDocument.m
+//  Changes
+//
+//  Created by Christoph on 25.02.2010.
+//  Copyright 2010 Useless Coding. All rights reserved.
+//
+
+#import "UCChangesDocument.h"
+
+@implementation UCChangesDocument
+
+- (id)init
+{
+    if(self = [super init])
+		{
+		}
+	return self;
+}
+
+- (NSString *)windowNibName
+{
+	return @"ChangesDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *) aController
+{
+	[super windowControllerDidLoadNib:aController];
+}
+
+- (BOOL)readFromURL:(NSURL *)absoluteURL ofType:(NSString *)typeName error:(NSError **)outError
+{
+	BOOL isFolder = NO;
+	if(![[NSFileManager defaultManager] fileExistsAtPath:[absoluteURL path] isDirectory:&isFolder] || isFolder)
+		{
+		if(outError!=NULL)
+			{
+			*outError = [NSError errorWithDomain:@"UCChangesErrors" code:UC_FOLDER_ERROR userInfo:[NSDictionary dictionaryWithObjectsAndKeys:
+				NSLocalizedString(@"Changes cannot be recorded for a folder.", @"Unable to open folder reason"), NSLocalizedFailureReasonErrorKey,
+				NSLocalizedString(@"Try opening a file by choosing the Open command from the File menu.", @"Unable to open folder suggestion"), NSLocalizedRecoverySuggestionErrorKey,
+				nil]];
+			}
+		return NO;
+		}
+    return YES;
+}
+
+@end
